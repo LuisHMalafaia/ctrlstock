b55384a10e5cc6f4e787220ea24c6c2c9d4d28b2
Luis Henrique de Oliveira Malafaia <luishmalafaia@gmail.com>
2024-10-30 16:47:25 -0300

    initial commit

---
+++ src/main/java/br/com/luishmalafaia/ctrlstock/CtrlstockApplication.java
@@ -0,0 +1,13 @@
+package br.com.luishmalafaia.ctrlstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luishmalafaia/ctrlstock/CtrlstockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luishmalafaia.ctrlstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
